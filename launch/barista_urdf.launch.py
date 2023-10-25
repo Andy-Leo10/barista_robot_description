63f990ac38d1b20625cd4a4aa500e976f039bca4
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2023-10-25 15:39:35 +0000

    3 launches generated to test the robot's deploy

--- launch/barista_urdf.launch.py
+++ launch/barista_urdf.launch.py
@@ -1,7 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_prefix
@@ -14,15 +14,27 @@ def generate_launch_description():
     description_package_name = "barista_robot_description"
     urdf_file = 'barista_robot_model.urdf'
     rviz_file='vis1.rviz'
+    install_dir=get_package_share_directory(description_package_name)
+    world_selected='obstacles.world'
     ####### DATA INPUT END ##########
     print("Fetching URDF ==>")
-    robot_desc_path = os.path.join(get_package_share_directory(description_package_name), "urdf", urdf_file)
+    robot_desc_path = os.path.join(install_dir, "urdf", urdf_file)
 
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-        launch_arguments={"verbose": "false", 'pause': 'true'}.items(),
-    )
+    # This is to find the models inside the models folder in my_robot_gazebo package
+    gazebo_models_path = os.path.join(description_package_name, 'meshes')
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] =  install_dir + "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("\nGAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"])+'\n')
+
 
     # Robot State Publisher
     robot_state_publisher_node = Node(
@@ -34,18 +46,10 @@ def generate_launch_description():
         output="screen"
     )
 
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py', name='spawn_entity_node',
                         arguments=['-entity', 'barista', '-x', '1.0', '-y', '1.0', '-z', '0.2',
-                                   '-topic', 'robot_description'],
+                                   '-topic', '/robot_description'],
                         output='screen')
-
-    # Joint state publisher GUI node            
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui_node',
-        output='screen'
-    )
 
     # RVIZ Configuration
     rviz_config_dir = os.path.join(get_package_share_directory(description_package_name), 'rviz', rviz_file)
@@ -56,12 +60,15 @@ def generate_launch_description():
             output='screen',
             name='rviz_node',
             parameters=[{'use_sim_time': True}],
-            arguments=['-d', rviz_config_dir])
+            arguments=['-d', rviz_config_dir])    
+
 
     return LaunchDescription([
-        #joint_state_publisher_gui_node,
-        gazebo,
         robot_state_publisher_node,
-        spawn_entity,
-        rviz_node
+        rviz_node,
+        DeclareLaunchArgument(
+                'world',
+                default_value=[os.path.join(install_dir, 'worlds', world_selected), ''],
+                description='SDF world file'),
+        spawn_entity
     ])
--- launch/visualize_rviz.launch.py
+++ launch/visualize_rviz.launch.py
@@ -51,8 +51,8 @@ def generate_launch_description():
     # create and return launch description object
     return LaunchDescription(
         [   
-            joint_state_publisher_gui_node,
             robot_state_publisher_node,
+            joint_state_publisher_gui_node,
             rviz_node
         ]
     )
